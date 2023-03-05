cd1b4d953c1b018c70b28346fd81bf902bf2989b
Tomek K <tomasz.kuncio@gmail.com>
2023-03-05 15:55:21 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby soltion for frontedmentor`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
