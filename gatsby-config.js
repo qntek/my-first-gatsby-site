ce50080bfe62b503138788df025e82527794bf1c
Tomek K <tomasz.kuncio@gmail.com>
2023-03-06 22:16:46 +0100

    End of tutorials part 4.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
 		title: `My Gatsby soltion for frontedmentor`,
 		siteUrl: `https://www.yourdomain.tld`,
 	},
-	plugins: ['gatsby-plugin-sass', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
+	plugins: [
+		'gatsby-plugin-sass',
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: `blog`,
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,30 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import Seo from '../components/Seo';
 
-const BlogPage = () => {
-  return (
-    <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
-    </Layout>
-  )
-}
+const BlogPage = ({ data }) => {
+	return (
+		<Layout pageTitle='My Blog Posts'>
+			<ul>
+				{data.allFile.nodes.map((node) => (
+					<li key={node.name}>{node.name}</li>
+				))}
+			</ul>
+		</Layout>
+	);
+};
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const query = graphql`
+	query {
+		allFile(filter: {sourceInstanceName: {eq:"blog"}}) {
+			nodes {
+				name
+			}
+		}
+	}
+`;
 
-export default BlogPage
+export const Head = () => <Seo title='My Blog Posts' />;
+
+export default BlogPage;
