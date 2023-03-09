ff5bcaade6b93cbdfab53952e8669cbfd74a11d2
Tomek K <tomasz.kuncio@gmail.com>
2023-03-09 18:34:47 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,29 +1,34 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
-import Seo from '../components/Seo';
+import { graphql, Link } from 'gatsby';
+import Layout from '../../components/layout';
+import Seo from '../../components/Seo';
 
 const BlogPage = ({ data }) => {
 	return (
 		<Layout pageTitle='My Blog Posts'>
 			{data.allMdx.nodes.map((node) => (
 				<article key={node.id}>
-					<h2>{node.frontmatter.title}</h2>
-					<p>Posted: {node.frontmatter.date}</p>
-					<p>{node.excerpt}</p>
+					<h2>
+						<Link to={`/blog/${node.frontmatter.slug}`}>
+							{node.frontmatter.title}
+						</Link>
+						<p>Posted: {node.frontmatter.date}</p>
+					</h2>
 				</article>
 			))}
+			
 		</Layout>
 	);
 };
 
 export const query = graphql`
 	query {
-		allMdx(sort: {frontmatter: {date: DESC}}) {
+		allMdx(sort: { frontmatter: { date: DESC } }) {
 			nodes {
 				frontmatter {
 					date
 					title
+					slug
 				}
 				id
 				excerpt
